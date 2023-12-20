72784fcf2fa39cafdaaadeeb23a02da310181f6a
chiaramaccani <maccanichiara@gmail.com>
2023-12-20 14:42:51 +0100

    Aperture problem

--- lossmap_LHC.py
+++ lossmap_LHC.py
@@ -209,8 +209,6 @@ def main():
         everest_colls = [name for name in coll_names if name not in black_absorbers]
         coll_manager.install_everest_collimators(names=everest_colls,verbose=True)
         coll_manager.install_black_absorbers(names = black_absorbers, verbose=True)
-
-
     else:
         raise ValueError(f"Unknown scattering engine {engine}!")
 
